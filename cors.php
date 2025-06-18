82dceef340fbdb8e9ece7cac7037e3b899c454ba
Phan Xuan Tien <xuantiengitlab.cntt@gmail.com>
2025-06-18 15:57:06 +0700

    Fix CORS headers in backend

--- cors.php
+++ cors.php
@@ -1,7 +1,7 @@
 <?php
 // backend_railway/cors.php
 
-header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Origin: https://websitetlu-frontend-.onrender.com");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
 header("Content-Type: application/json; charset=UTF-8"); 
